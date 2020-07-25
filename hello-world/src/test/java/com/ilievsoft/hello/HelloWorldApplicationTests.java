fa4faa7e22d305e747063720bc6723e9a0c718e4
Hristo ILIEV <hristo.iliev@ilievsoft.com>
2020-07-25 21:20:19 +0200

    Init commit

---
+++ hello-world/src/test/java/com/ilievsoft/hello/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilievsoft.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
